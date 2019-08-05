004e4a836b0aab9d1eca2b9efdf6077d72ffea66
Erlend Åmdal <erlend@aamdal.com>
2019-08-05 22:54:09 +0200

    Use reach-entity-attributes

--- src/main/java/com/jamieswhiteshirt/hardhatsteve/common/entity/attribute/HardHatSteveEntityAttributes.java
+++
@@ -1,8 +0,0 @@
-package com.jamieswhiteshirt.hardhatsteve.common.entity.attribute;
-
-import net.minecraft.entity.attribute.ClampedEntityAttribute;
-import net.minecraft.entity.attribute.EntityAttribute;
-
-public class HardHatSteveEntityAttributes {
-    public static final EntityAttribute REACH = (new ClampedEntityAttribute(null, "generic.hard-hat-steve.reach", 0.0D, 0.0D, 2.0D)).setName("Reach").setTracked(true);
-}
